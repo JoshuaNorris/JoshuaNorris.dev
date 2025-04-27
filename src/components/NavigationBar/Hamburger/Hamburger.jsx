24a06ff9337bb576b7f192d85e6f48d25ebd7318
Joshua Norris <joshua.norris52@gmail.com>
2025-04-27 12:30:45 -0500

    nothing works

--- src/components/NavigationBar/Hamburger/Hamburger.jsx
+++ src/components/NavigationBar/Hamburger/Hamburger.jsx
@@ -2,19 +2,18 @@ import './Hamburger.styles.scss';
 
 export default function Hamburger() {
     return (
-        <nav className='burger-nav'>
-            <div id="menuToggle">
-                <input id='burger-input' type="checkbox" />
-                    <span className='burger' id='top_burger'></span>
-                    <span className='burger' id='middle_burger'></span>
-                    <span className='burger' id='bottom_burger'></span>
-                <ul id="menu">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Info</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-            </div>
-        </nav>  
+        <nav id="hamburger-navigation">
+        <input type="checkbox" />
+        <span className="burger"></span>
+        <span className="burger"></span>
+        <span className="burger"></span>
+  
+        <ul id="menu">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Info</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
     );
 }
